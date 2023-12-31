0516ead9f6af9ceb5b14fc91086c9b0b33cd61db
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-01 04:21:32 +0530

    new fun

--- src/Person.java
+++ src/Person.java
@@ -2,9 +2,12 @@ package src;
 
 public class Person {
     int age =20;
+    String data;
 
     public static void main(String[] args) {
         Person obj =new Person();
         System.out.println(obj.age);
+        String data=new String();
+        System.out.println(data);
     }
 }
