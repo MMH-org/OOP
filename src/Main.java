81a55389927f10245661a420597906faa8ea5283
Mohsin <mdmohsinhaider6@gmail.com>
2023-12-31 23:57:07 +0530

    new year special commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         System.out.println("Hello from main");
+        System.out.println("new year ");
     }
 
 }
