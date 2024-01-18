5287df2c5dc1610184056b7c84af24fa35b4b0d8
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-18 23:53:22 +0530

    initl

--- src/ReverseArrays.java
+++ src/ReverseArrays.java
@@ -3,15 +3,5 @@ package src;
 import java.util.Scanner;
 
 public class ReverseArrays {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("how many elements you want ?");
-        int n = sc.nextInt();
-        int[]elt=new int[n];
 
-        for (int i=0; i<n;i++){
-            System.out.println("Enter array element " +(i+1));
-        }
-
-    }
 }
