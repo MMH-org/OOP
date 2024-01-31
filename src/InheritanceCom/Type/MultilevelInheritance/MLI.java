870644fdfc233a7df4208adcdbe7222d6396433f
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-01 02:38:32 +0530

    Multilevel Inheritance

--- src/InheritanceCom/Type/MultilevelInheritance/MLI.java
+++ src/InheritanceCom/Type/MultilevelInheritance/MLI.java
@@ -3,6 +3,7 @@ package src.InheritanceCom.Type.MultilevelInheritance;
 public class MLI {
     public static void main(String[] args) {
         equilateralTriangle equilateralTriangle_obj=new equilateralTriangle();
+
         equilateralTriangle_obj.triangleMethod();
         equilateralTriangle_obj.triangleMethod();
         equilateralTriangle_obj.shapeMethod();
