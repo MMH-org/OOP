c5281cd9372980aa3a05e5a8b5fc24c1f4bc36e1
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-01 03:02:44 +0530

    Hybrid Inheritance

--- src/InheritanceCom/Type/HybridInheritance/Hybrid.java
+++ src/InheritanceCom/Type/HybridInheritance/Hybrid.java
@@ -3,5 +3,7 @@ package src.InheritanceCom.Type.HybridInheritance;
 public class Hybrid {
     public static void main(String[] args) {
         System.out.println("Hybrid Inheritance");
+        String notes="Hybrid inheritance is a combination of two or more types of inheritance";
+        System.out.println(notes);
     }
 }
