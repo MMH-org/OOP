6d095a6d0a50c4c7c4093814b8f07e27d9043e50
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-01 02:27:07 +0530

    single level inheritance in java

--- src/InheritanceCom/Type/SingleLevelInheritance/Tringle.java
+++ src/InheritanceCom/Type/SingleLevelInheritance/Tringle.java
@@ -3,7 +3,8 @@ package src.InheritanceCom.Type.SingleLevelInheritance;
 public class Tringle extends Shape{
     public void area(int l,int b){
         System.out.println("triangle class");
-        System.out.println(1/2*l*b);
+        int A=l*b;
+        System.out.println(A/2);
     }
 }
 
