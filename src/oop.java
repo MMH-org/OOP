c655dba7d54109d94655a3061dfcf38410663ab6
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-31 23:13:35 +0530

    non parametric constructors

--- src/oop.java
+++ src/oop.java
@@ -20,20 +20,29 @@ class Student_{
         System.out.println(this.name);
         System.out.println(this.age);
     }
+    Student_(){
+        System.out.println("non parametric constructors");
+    }
 }
 
 public class oop {
     public static void main(String[] args) {
-        Pen pen1 = new Pen();
-        pen1.color="blue";
-        pen1.type="gel";
+//        Pen pen1 = new Pen();
+//        pen1.color="blue";
+//        pen1.type="gel";
+//
+////        pen1.write();
+//        Pen pen2 = new Pen();
+//        pen2.color="black";
+//        pen2.type="ball Pen";
+//
+//        pen1.printColor();
+//        pen2.printColor();
 
-//        pen1.write();
-        Pen pen2 = new Pen();
-        pen2.color="black";
-        pen2.type="ball Pen";
-
-        pen1.printColor();
-        pen2.printColor();
+        // class Student_
+        Student_ s1=new Student_();
+        s1.name="Devil";
+        s1.age=22;
+        s1.printInfo();
     }
 }
