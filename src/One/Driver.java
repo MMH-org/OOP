4069df80c9a0f5a28ad31b003efe87793edbcdfb
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-28 23:43:33 +0530

    property and methods

---
+++ src/One/Driver.java
@@ -0,0 +1,13 @@
+package src.One;
+
+public class Driver {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        // Car is a class name
+        // myCar is object references that is store objects address
+        // new is a keywords that is use for creating an object
+        // Car()  is a constructions
+
+        myCar.drive();
+    }
+}
